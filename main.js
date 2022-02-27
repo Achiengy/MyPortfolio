c0f6370f3730c4ea9d8f4cdc3c3a972c820e58ee
Achiengy <hope.pete@student.moringaschool.com>
2022-02-27 15:43:59 +0300

    ft:scroll up button-This commit will add a scroll up button

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
+   $(window).scroll(function(){
+
+   if(this.scroll > 500){
+    $('.scroll-up-button').addClass("show");
+   }else{
+    $('.scroll-up-button').addRemove("show");
+   }
+});
+
+  // Slide-up 
+  $('.scroll-up-button').click(function(){
+     $('html').animated({scrollTop: 0});
+  });
+
    
-    // toggle menu/navbar script 
+    // toggle menu/navbar 
     $('.menu-btn').click(function(){
       $('.navbar .menu').toggleClass("active");
       $('.menu-btn i').toggleClass("active");
     });
-    
+
 });
