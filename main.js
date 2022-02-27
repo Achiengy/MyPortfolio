322bd2be376e6fdce8e28a73baa479c422b15f42
Achiengy <hope.pete@student.moringaschool.com>
2022-02-27 17:44:16 +0300

    ft:homepage-This commit will add the typing text animation on the home page

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
-   $(window).scroll(function(){
-
-   if(this.scroll > 500){
-    $('.scroll-up-button').addClass("show");
-   }else{
-    $('.scroll-up-button').addRemove("show");
-   }
-});
-
-  // Slide-up 
-  $('.scroll-up-button').click(function(){
-     $('html').animated({scrollTop: 0});
+  $(window).scroll(function(){
+     
+ // scroll-up button show/hide 
+      if(this.scrollY > 500){
+          $('.scroll-up-button').addClass("show");
+      }else{
+          $('.scroll-up-button').removeClass("show");
+      }
   });
 
-   
-    // toggle menu/navbar 
-    $('.menu-btn').click(function(){
+  // slide-up 
+  $('.scroll-up-button').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click
+      $('html').css("scrollBehavior", "auto");
+  });
+
+  $('.navbar .menu li a').click(function(){
+      //smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar
+  $('.menu-btn').click(function(){
       $('.navbar .menu').toggleClass("active");
       $('.menu-btn i').toggleClass("active");
-    });
+  });
 
+// typing text animation
+    var typed = new Typed(".typing", {
+        strings: ["Full Stack Web Developer"],
+        typeSpeed: 90,
+        backSpeed: 50,
+        loop: true
+    });
 });
